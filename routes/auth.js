dc23494f7524ea7eefd084928dbe5affb11bbca3
Samvel Grigoryan <savelgrig666@agmail.com>
2025-10-19 11:01:11 +0400

    Created POST request for registration

--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,74 @@
-const {Router} = require('express')
-const User = require("../models/user")
-const router = Router()
+const { Router } = require("express");
+const User = require("../models/user");
+const router = Router();
 
-router.get("/login", async (req, res) =>{
-    res.render("auth/login", {
-        title: "Login page",
-        isLogin: true
-    })
-})
+router.get("/login", async (req, res) => {
+  res.render("auth/login", {
+    title: "Login page",
+    isLogin: true,
+  });
+});
 
-router.get("/logout", async (req, res) =>{
-    req.session.destroy(() => {
-        res.redirect("/")
-    })
-})
+router.get("/logout", async (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
+});
 
 router.post("/login", async (req, res) => {
-    const user = await User.findById("68d9122d992642a6b66d0570");
-    req.session.user = user
-    req.session.isAuthenticated = true
-    req.session.save(err => {
-        if (err) {
-            throw err
-        }
+  try {
+    const { email, password } = req.body;
 
-        res.redirect("/")
-    })
-    
-})
+    const candidate = await User.findOne({ email });
 
-module.exports = router
+    if (candidate) {
+      const areSome = password === candidate.password;
+      if (areSome) {
+        req.session.user = candidate;
+        req.session.isAuthenticated = true;
+        req.session.save((err) => {
+          if (err) {
+            throw err;
+          }
+
+          res.redirect("/");
+        });
+      } else {
+        res.redirect("/auth/login#register");
+      }
+    } else {
+      res.redirect("/auth/login#register");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    const { reg_email, name, reg_password, confirm } = req.body;
+
+    const candidate = await User.findOne({ email: reg_email });
+
+    if (reg_password !== confirm) {
+      return res.status(400).send("sxala bratt");
+    }
+
+    if (candidate) {
+      res.redirect("/auth/login");
+    } else {
+      const user = new User({
+        email: reg_email,
+        name,
+        password: reg_password,
+        card: { items: [] },
+      });
+      await user.save();
+      res.redirect("/auth/login#login");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+module.exports = router;
