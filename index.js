97dfee5d91afbfce5065e4001da2fa1a978f0d79
Samvel Grigoryan <savelgrig666@agmail.com>
2025-10-22 20:32:09 +0400

    Require the connect-flash package and add it as middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const csrf = require("csurf")
+const flash = require("connect-flash")
 const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
 const session = require("express-session")
@@ -47,7 +48,7 @@ app.use(session({
 }))
 
 app.use(csrf())
-
+app.use(flash())
 app.use(varMiddleware)
 app.use(userMiddleware)
 
